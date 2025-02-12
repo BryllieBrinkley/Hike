4fa4e5e5beeee85c9366bc2eebbad70cd55ba09c
Jibryll Brinkley <jibryllbrink@yahoo.com>
2025-02-12 17:50:13 -0500

    Initial Commit

---
+++ Hike/HikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikeApp.swift
+//  Hike
+//
+//  Created by Jibryll Brinkley on 2/12/25.
+//
+
+import SwiftUI
+
+@main
+struct HikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

40dec47ec491cff31be43dc617b7418374a68d8c
Jibryll Brinkley <jibryllbrink@yahoo.com>
2025-02-12 18:26:27 -0500

    added card component + extension for custom colors

---
+++ Hike/Component/CustomBackgroundView.swift
@@ -0,0 +1,41 @@
+//
+//  CustomBackgroundView.swift
+//  Hike
+//
+//  Created by Jibryll Brinkley on 2/12/25.
+//
+
+import SwiftUI
+
+struct CustomBackgroundView: View {
+    
+    var body: some View {
+        ZStack {
+            
+            // 3. DEPTH
+            Color.customGreenDark
+                .cornerRadius(40)
+                .offset(y: 12)
+
+            // 2. Light
+            Color.customGreenLight
+                .cornerRadius(40)
+                .offset(y: 3)
+                .opacity(0.85)
+            
+            // 1.Surface
+            LinearGradient(colors: [
+                Color.customGreenLight,
+                Color.customGreenMedium],
+                           startPoint: .top,
+                           endPoint: .bottom
+            )
+            .cornerRadius(40)
+        }
+    }
+}
+
+#Preview {
+    CustomBackgroundView()
+        .padding()
+}
---
+++ Hike/Utility/ColorExt.swift
@@ -0,0 +1,20 @@
+//
+//  ColorExt.swift
+//  Hike
+//
+//  Created by Jibryll Brinkley on 2/12/25.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    
+    static let customGrayLight = Color("ColorGrayLight")
+    static let customGrayMedium = Color("ColorGrayMedium")
+    static let customGreenLight = Color("ColorGreenLight")
+    static let customGreenMedium = Color("ColorGreenMedium")
+    static let customGreenDark = Color("ColorGreenDark")
+    static let customIndigoMedium = Color("ColorIndigoMedium")
+    static let customSalmonLight = Color("ColorSalmonLight")
+}

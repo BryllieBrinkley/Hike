8a98d71c033dfa2ac3850e0d8e98b282d5697b34
Jibryll Brinkley <jibryllbrink@yahoo.com>
2025-02-12 19:15:10 -0500

    Add card header + custom button

--- Hike/Component/CardView.swift
+++ Hike/Component/CardView.swift
@@ -11,23 +11,59 @@ struct CardView: View {
     var body: some View {
         ZStack {
             CustomBackgroundView()
-            ZStack {
-                Circle()
-                    .fill(
-                        LinearGradient(
-                            colors: [Color.customIndigoMedium, Color.customSalmonLight],
-                            startPoint: .topLeading,
-                            endPoint: .bottomTrailing)
-                    )
-                    .frame(width: 256, height: 256)
-                Image("image-1")
-                    .resizable()
-                    .scaledToFit()
+            VStack {
+                // Header
+                VStack(alignment: .leading) {
+                    HStack {
+                        Text("Hiking")
+                            .fontWeight(.black)
+                            .font(.system(size: 52))
+                            .foregroundStyle(
+                                LinearGradient(
+                                    colors: [.customGrayLight, .customGrayMedium],
+                                    startPoint: .top,
+                                    endPoint: .bottom)
+                            )
+                        
+                        Spacer()
+                        
+                        Button {
+                            // Action: present a sheet
+                            print("button pressed")
+                        } label: {
+                            CustomButtonView()
+                        }
+                    }
+                    Text("Fun and enjoyable outdoor activity for friends and families.")
+                        .multilineTextAlignment(.leading)
+                        .italic()
+                        .foregroundColor(.customGrayMedium)
+                } // Header
+                .padding(.horizontal, 30)
+                
+                // Main Content
+                ZStack {
+                    
+                    
+                    Circle()
+                        .fill(
+                            LinearGradient(
+                                colors: [Color.customIndigoMedium, Color.customSalmonLight],
+                                startPoint: .topLeading,
+                                endPoint: .bottomTrailing)
+                        )
+                        .frame(width: 256, height: 256)
+                    Image("image-1")
+                        .resizable()
+                        .scaledToFit()
+                }
+                
+                // Footer
             }
             
         } // Card
         .frame(width: 320, height: 570)
-
+        
     }
 }
 
